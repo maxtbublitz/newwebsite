94341b2417e93bc605234afd68a4cd5c10993a1a
Max Bublitz <maxtbublitz.com>
2023-04-28 13:12:56 -0600

    Cards initial function added

---
+++ javascript/cards.js
@@ -0,0 +1,23 @@
+const card1 = document.getElementById("C1");
+card1.addEventListener("click",flipCard1);
+function flipCard1(){
+	card1.classList.toggle("flipCard");
+}
+
+const card2 = document.getElementById("C2");
+card2.addEventListener("click",flipCard2);
+function flipCard2(){
+	card2.classList.toggle("flipCard");
+}
+
+const card3 = document.getElementById("C3");
+card3.addEventListener("click",flipCard3);
+function flipCard3(){
+	card3.classList.toggle("flipCard");
+}
+
+const card4 = document.getElementById("C4");
+card4.addEventListener("click",flipCard4);
+function flipCard4(){
+	card4.classList.toggle("flipCard");
+}
